d3955d5f234cd32086266dc5ace2ccf221b622f9
zach pratt <zach@attackofzach.com>
2015-09-26 21:31:20 -0500

    Delete karma and eslint tasks. Run them from the shell instead.

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,11 @@
 
 var gulp = require('gulp'),
 
-    eslint = require('./gulp/eslint'),
-    karma = require('./gulp/karma'),
     previewServer = require('./gulp/preview-server'),
     webpack = require('./gulp/webpack');
 
-gulp.task('lint', eslint(gulp));
-gulp.task('karma', ['lint'], karma());
-gulp.task('webpack', ['karma'], webpack());
+gulp.task('webpack', webpack());
 gulp.task('server', previewServer());
 
-gulp.task('test', ['karma']);
-gulp.task('build', ['test', 'webpack']);
+gulp.task('build', ['webpack']);
 gulp.task('default', ['server']);
